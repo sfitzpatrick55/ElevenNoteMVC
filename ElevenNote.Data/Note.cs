19038405c81053dab4ef410b1b012e9f576cc128
Scott <sfitzpatrick55@gmail.com>
2022-06-11 14:49:44 -0400

    Initial commit

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Data
+{
+	public class Note
+	{
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Date Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Date Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; } // '?' means the modified UTC can be null
+    }
+}
