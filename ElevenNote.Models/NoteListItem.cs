b9bb7a7b4aaf9dd11361ec0f5290a6abd99e0a13
Scott <sfitzpatrick55@gmail.com>
2022-06-13 16:39:15 -0400

    Create NoteListItem

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Models
+{
+	public class NoteListItem
+	{
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
+
