e5b2dd855dd1dcd2e6aff1486c3002c1c680468d
Scott <sfitzpatrick55@gmail.com>
2022-06-13 16:39:27 -0400

    Create Note Controller

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElevenNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
+
