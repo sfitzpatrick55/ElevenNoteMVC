e3eb29f368f012bd2c2ce105e7d659af0eb961d4
Scott <sfitzpatrick55@gmail.com>
2022-06-14 18:02:38 -0400

    Add GET Create method to note controller

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ElevenNote.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ElevenNote.WebMVC.Controllers
 {
+
+    [Authorize]
+
     public class NoteController : Controller
     {
         // GET: /<controller>/
@@ -16,6 +20,12 @@ namespace ElevenNote.WebMVC.Controllers
         {
             var model = new NoteListItem[0];
             return View(model);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
