40adb1cac610398b30ebea5aef6f038be67eec2a
Scott <sfitzpatrick55@gmail.com>
2022-06-16 20:43:58 -0400

    Create NoteDetail model

---
+++ ElevenNote.Models/Note/NoteDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Models.Note
+{
+	public class NoteDetail
+	{
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name="Date Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name="Date Modified")]
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
+
