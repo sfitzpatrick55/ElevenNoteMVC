3885abf0b49750621ab7091cd80dda69269eef34
Scott <sfitzpatrick55@gmail.com>
2022-06-16 20:55:59 -0400

    Add GetNoteById method to NoteService

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -48,6 +48,22 @@ namespace ElevenNote.Services
                     );
                 return query.ToArray();
         }
+
+        public NoteDetail GetNoteById(int id)
+        {
+            var entity = _ctx
+                .Notes
+                .Single(e => e.NoteId == id && e.OwnerId == _userId);
+            return
+                new NoteDetail
+                {
+                    NoteId = entity.NoteId,
+                    Title = entity.Title,
+                    Content = entity.Content,
+                    CreatedUtc = entity.CreatedUtc,
+                    ModifiedUtc = entity.ModifiedUtc
+                };        
+        }
     }
 }
 
