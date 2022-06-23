dccda6c1b6f7ed625463bc36b08b264b66721b92
Scott <sfitzpatrick55@gmail.com>
2022-06-23 18:26:14 -0400

    Add WebApiConfig.cs

---
+++ ElevenNote.WebMVC/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace ElevenNote.WebMVC
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            GlobalConfiguration
+                .Configure(
+                    x =>
+                    {
+                        x
+                            .Formatters
+                            .JsonFormatter
+                            .SupportedMediaTypes
+                            .Add(new MediaTypeHeaderValue("text/html"));
+
+                        x.MapHttpAttributeRoutes();
+                    }
+                );
+        }
+    }
+}
